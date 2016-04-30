5c5542ab2c04cabb52a46a3f159f5a82aac2812c
Jan Zípek <jan@zipek.cz>
2016-04-30 11:43:00 +0200

    Polished build process. Enhanced linux execatuble.

--- dist-src/minicloud.sh
+++ dist-src/minicloud.sh
@@ -1,2 +1,16 @@
-#!/bin/sh
-java -jar Minicloud.jar "$@"
+#!/bin/bash
+
+## Author: Glenn Jackman
+## Modified by: SkaceKachna
+## Link: http://stackoverflow.com/a/7335524/2205567
+
+if type -p java > /dev/null; then
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "Failed to find java executable. Please install JRE."
+	exit 1
+fi
+
+$_java -jar ./minicloud.jar "$@"
